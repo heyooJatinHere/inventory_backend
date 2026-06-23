63c594c5299fdfbb3c55f678fb3be36fed9ab2a7
ixambeejatin <jatin.kumar@ixambee.com>
2026-06-23 23:02:56 +0000

    frontend url in cors

--- app/main.py
+++ app/main.py
@@ -12,7 +12,8 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173"
+        "http://localhost:5173",
+        "https://inventory-frontend-ir4voacon-heyoojatinheres-projects.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
